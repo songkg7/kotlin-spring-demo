4a173e6b67cb21c558783d4b70b417631dc9f7a9
kgsong <kyunggeun.song@seavantage.com>
2022-04-16 22:02:39 +0900

    Lambda test path

--- src/main/kotlin/com/example/kotlinspringpractice/mvc/controller/HomeController.kt
+++ src/main/kotlin/com/example/kotlinspringpractice/mvc/controller/HomeController.kt
@@ -27,4 +27,9 @@ class HomeController {
         return ResponseEntity.ok().body(person)
     }
 
+    @GetMapping("/lambda")
+    fun lambda(): String {
+        return "Hello, lambda!"
+    }
+
 }
